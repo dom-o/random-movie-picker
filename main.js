fdc737dee2756094bce058a4cb10a711a3d4b489
Domonic Milesi <dom-o@users.noreply.github.com>
2019-04-26 01:10:23 -0400

    Change random selection so no repeats until every movie's been picked once. Add some minor ability to alert user to errors.

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-function getRandom(arr) {
-  len = arr.length;
-  rand =  Math.floor(Math.random() * len);
-  return arr[rand];
-}
-
-function injectMovie(movie) {
+function injectMovie(movie, errorText) {
   var movieDiv = document.getElementById('movie-info');
 
-  var newHtml =
+  var newHtml = (movie) ?
     '<img id="movie-poster" src="' + movie.images[0].url + '"' +
       'alt="Poster for ' + movie.title + ' (' + movie.year + ')" height="250" width="167"/>' +
     '<h2 id="movie-title">' + movie.title + ' (' + movie.year + ')</h2>' +
@@ -16,19 +10,42 @@ function injectMovie(movie) {
       'src=' + '"https://www.youtube.com/embed/' + movie.youTubeTrailerId + '"' +
       'frameborder="0"' +
       'allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>' +
-    '</iframe>';
-    movieDiv.innerHTML = newHtml;
+    '</iframe>'
+  : '<p>'+errorText+'</p>';
+  movieDiv.innerHTML = newHtml;
+}
+
+function shuffle(arr) {
+  for(i=arr.length-1; i>=0; i--) {
+    j = Math.floor(Math.random() * (arr.length));
+
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+}
+
+function pickMovie() {
+  if(cursor >= movies.length) {
+    cursor = 0;
+    movies = shuffle(movies);
+  }
+
+  // TODO: validate movie according to schema before injecting
+  injectMovie(movies[cursor],
+    'No movies available. Your radarr list might be empty, or the server erroneously gave me an empty list.');
+  cursor++;
+  localStorage.setItem('cursor', JSON.stringify(cursor));
 }
 
 function setup(movies) {
-  movie = getRandom(movies);
-  injectMovie(movie);
-  document.getElementById('movie-pick').onclick = function() {
-    injectMovie(getRandom(movies));
-  };
+  pickMovie();
+  document.getElementById('movie-pick').onclick = pickMovie;
 }
 
 var movies = JSON.parse(localStorage.getItem('movies'));
+var cursor = (movies && localStorage.getItem('cursor')) ? JSON.parse(localStorage.getItem('cursor')) : 0;
 
 var needsSetup = true;
 if(movies) {
@@ -37,17 +54,37 @@ if(movies) {
 }
 
 var request = new XMLHttpRequest();
-request.open('GET', requestUrl+apiKey);
+request.open('GET', requestUrl);
 
 request.responseType = 'json';
 request.send();
 
 request.onload = function() {
+  watchedMovies = (movies && cursor>0) ? movies.slice(0, cursor) : null;
+
   movies = request.response;
-  movies = movies.filter(movie => movie.downloaded && movie.hasFile);
-  localStorage.setItem('movies', JSON.stringify(movies));
-  if(needsSetup) {
-    setup(movies);
-    needsSetup=false;
+  if(movies){
+    movies = movies.filter(movie => movie.downloaded && movie.hasFile);
+    if(watchedMovies) {
+      for(i=0;i<watchedMovies.length;i++) {
+        ind = movies.findIndex(movie => movie.id===watchedMovies[i].id);
+        if(ind === -1) {
+          watchedMovies.splice(i, 1);
+          i--;
+        } else {
+          movies.splice(ind, 1);
+        }
+      }
+    }
+    movies = shuffle(movies);
+
+    if(watchedMovies) { movies = watchedMovies.concat(movies); }
+    localStorage.setItem('movies', JSON.stringify(movies));
+    if(needsSetup) {
+      setup(movies);
+      needsSetup=false;
+    }
+  } else {
+    injectMovie(null, 'No movies available. This might be a server issue; I got ' +request.status + ' ' + request.statusText + '. Try refreshing the page.');
   }
 };
